bcf931c63822fe67bbb2a41d228656024b91efad
saisushma <chilukurisaisushma@gmail.com>
2024-12-09 23:08:23 +0530

    intiial

---
+++ src/main/java/com/thetestingacademy/endPoints/APIConstants.java
@@ -0,0 +1,10 @@
+package com.thetestingacademy.endPoints;
+
+public class APIConstants {
+    public static String Base_URL ="https://restful-booker.herokuapp.com/";
+//    public static String Base_URL = fetch thsi csv/execl;
+    public static String Create_upadte_booking_url = "/booking";
+    public static String auth_url = "/auth";
+    public static String ping_irl = "/ping";
+
+}
--- src/test/java/com/thetestingacademy/tests/integration/Test001.java
+++ src/test/java/com/thetestingacademy/tests/integration/Test001.java
@@ -1,4 +1,14 @@
 package com.thetestingacademy.tests.integration;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
 public class Test001 {
+
+
+@Test
+    public void test_hello_ra()
+{
+    RestAssured.given();
+}
 }
